3278fbfd11d7c11cf312d6c810b7e974540d45f9
HelloHello <anton08@gmx.de>
2025-02-19 08:00:57 +0100

    added colored all the minecraft colors

--- src/main/java/io/github/foundationgames/splinecart/track/TrackColor.java
+++ src/main/java/io/github/foundationgames/splinecart/track/TrackColor.java
@@ -6,11 +6,23 @@ import org.joml.Vector3f;
 
 public enum TrackColor {
 
-    WHITE(1, 1,1, DyeColor.WHITE),
+    WHITE(0xffffff, DyeColor.WHITE),
+    LIGHT_GRAY(0xc6c6c6, DyeColor.LIGHT_GRAY),
+    GREY(0x636362, DyeColor.GRAY),
     BLACK(0, 0, 0, DyeColor.BLACK),
+    BROWN(0x7f5b00, DyeColor.BROWN),
     RED(1, 0, 0, DyeColor.RED),
-    GREEN(0, 1, 0, DyeColor.GREEN),
-    BLUE(0, 0, 1, DyeColor.BLUE);
+    ORANGE(0xfca905, DyeColor.ORANGE),
+    YELLOW(0xffff00, DyeColor.YELLOW),
+    LIME(0x00ff00, DyeColor.LIME),
+    GREEN(0x04ce04, DyeColor.GREEN),
+    CYAN(0x02fcad, DyeColor.CYAN),
+    LIGHT_BLUE(0x00f7e2, DyeColor.LIGHT_BLUE),
+    BLUE(0, 0, 1, DyeColor.BLUE),
+    PURPLE(0xd202fc, DyeColor.PURPLE),
+    MAGENTA(0xef02fc, DyeColor.MAGENTA),
+    PINK(0xfc02eb, DyeColor.PINK),
+    ;
 
     public final float r;
     public final float g;
@@ -22,6 +34,13 @@ public enum TrackColor {
         this.g = g;
         this.b = b;
         this.item = item;
+    }
+
+    TrackColor(int hex, DyeColor item) {
+        this.item = item;
+        r = (float) ((hex & 0xff0000) >> 16) / 0xff;
+        g = (float) ((hex & 0x00ff00) >> 8) / 0xff;
+        b = (float) ((hex & 0x0000ff)) / 0xff;
     }
 
 
