5a46d2566ba814984a83d44944244a6d199ba32e
HelloHello <anton08@gmx.de>
2025-03-08 17:08:52 +0100

    two line description for tools is displayed

--- src/main/java/io/github/foundationgames/splinecart/item/RegistrableItem.java
+++ src/main/java/io/github/foundationgames/splinecart/item/RegistrableItem.java
@@ -8,13 +8,26 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class RegistrableItem extends Item {
 
     public RegistrableItem(String identifier, RegistryKey<Item> registryKey) {
         super(new Item.Settings().component(DataComponentTypes.LORE,
-                new LoreComponent(List.of((Text.translatable("item." + Splinecart.MOD_NAME + "." + identifier + ".desc").formatted(Formatting.GRAY))))).registryKey(registryKey));
+                new LoreComponent(loadDescription("item." + Splinecart.MOD_NAME + "." + identifier))).registryKey(registryKey));
+    }
+
+    /**
+     *
+     * @param path the path in the lang file (e.g. item.splinecart.test_item), without .desc in the end.
+     * @return
+     */
+    public static List<Text> loadDescription(String path) {
+        List<Text> text = new ArrayList<>();
+        text.add(Text.translatable(path + ".desc").formatted(Formatting.GRAY));
+        text.add(Text.translatable(path + ".desc1").formatted(Formatting.GRAY));
+        return text;
     }
 
 }
