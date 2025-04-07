2af4f6c9ca0f5e365d16c92fa0b1a9254e66d0fb
HelloHello <anton08@gmx.de>
2025-04-07 12:56:29 +0200

    v0.12 add redstone comparator output for power level; add redstone input to execute triggers

--- src/main/java/io/github/foundationgames/splinecart/block/TrackMarkerBlock.java
+++ src/main/java/io/github/foundationgames/splinecart/block/TrackMarkerBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.block.WireOrientation;
 import org.jetbrains.annotations.Nullable;
 
 public class TrackMarkerBlock extends Block implements BlockEntityProvider {
@@ -17,6 +18,8 @@ public class TrackMarkerBlock extends Block implements BlockEntityProvider {
     public static final MapCodec<TrackMarkerBlock> CODEC = createCodec(TrackMarkerBlock::new);
 
     public static final int OUTLINE_SHAPE_MARGIN = 2;
+
+    private boolean redstonePowered = false;
 
     public TrackMarkerBlock(Settings settings) {
         super(settings);
@@ -45,6 +48,31 @@ public class TrackMarkerBlock extends Block implements BlockEntityProvider {
         }
 
         super.onStateReplaced(state, world, pos, newState, moved);
+    }
+
+    @Override
+    protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, @Nullable WireOrientation wireOrientation, boolean notify) {
+        boolean receivedPower = world.getReceivedRedstonePower(pos) != 0;
+        if(!redstonePowered && receivedPower) {
+            if (world.getBlockEntity(pos) instanceof TrackMarkerBlockEntity marker) {
+                marker.triggers.execute(world);
+            }
+        }
+        redstonePowered = receivedPower;
+    }
+
+    @Override
+    protected boolean hasComparatorOutput(BlockState state) {
+        return true;
+    }
+
+    @Override
+    protected int getComparatorOutput(BlockState state, World world, BlockPos pos) {
+        if (world.getBlockEntity(pos) instanceof TrackMarkerBlockEntity marker) {
+            int markerPower = marker.getPower();
+            return Math.abs(markerPower == Integer.MAX_VALUE ? 0 : Math.abs(markerPower / 10));
+        }
+        return 0;
     }
 
     @Override
