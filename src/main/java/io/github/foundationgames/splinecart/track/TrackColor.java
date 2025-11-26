b2857245f41343d1cff95745fca842827304bd38
Martin <66840021+Mcrtin@users.noreply.github.com>
2025-11-26 12:10:26 +0100

    refactor: use javas color instead of own

--- src/main/java/io/github/foundationgames/splinecart/track/TrackColor.java
+++ src/main/java/io/github/foundationgames/splinecart/track/TrackColor.java
@@ -1,38 +1,47 @@
 package io.github.foundationgames.splinecart.track;
 
-import org.joml.Vector3f;
+import net.minecraft.util.DyeColor;
+
+import java.awt.*;
 
 /**
- * Represents an RGB color that the track can have.
- * @param hex format: 0x00RRGGBB
+ * Contains a specific RGB color for every minecraft dye.
  */
-public record TrackColor(int hex) {
+public enum TrackColor {
 
-    public float getR() {
-        return (float) ((hex & 0xff0000) >> 16) / 0xff;
+    WHITE(0xffffff, DyeColor.WHITE),
+    LIGHT_GRAY(0xc6c6c6, DyeColor.LIGHT_GRAY),
+    GREY(0x636362, DyeColor.GRAY),
+    BLACK(0x202020, DyeColor.BLACK),
+    BROWN(0x7f5b00, DyeColor.BROWN),
+    RED(0xff0000, DyeColor.RED),
+    ORANGE(0xfca905, DyeColor.ORANGE),
+    YELLOW(0xffff00, DyeColor.YELLOW),
+    LIME(0x00ff00, DyeColor.LIME),
+    GREEN(0x04ce04, DyeColor.GREEN),
+    CYAN(0x02fcad, DyeColor.CYAN),
+    LIGHT_BLUE(0x00f7e2, DyeColor.LIGHT_BLUE),
+    BLUE(0x0000ff, DyeColor.BLUE),
+    PURPLE(0xd202fc, DyeColor.PURPLE),
+    MAGENTA(0xef02fc, DyeColor.MAGENTA),
+    PINK(0xfc02eb, DyeColor.PINK),
+    ;
+
+    public final Color color;
+    public final DyeColor item;
+
+    TrackColor(int hex, DyeColor item) {
+        this.color = new Color(hex);
+        this.item = item;
     }
 
-    public float getG() {
-        return (float) ((hex & 0x00ff00) >> 8) / 0xff;
-    }
-
-    public float getB() {
-        return (float) (hex & 0x0000ff) / 0xff;
-    }
-
-    /**
-     *
-     * @return a Vec3f with the r, g, and b values in its fields.
-     */
-    public Vector3f getVec3f() {
-        return new Vector3f(getR(), getG(), getB());
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof TrackColor(int other)) {
-            return hex == other;
+    public static TrackColor valueOf(DyeColor color) {
+        for(TrackColor trackColor : TrackColor.values()) {
+            if(color == trackColor.item) {
+                return trackColor;
+            }
         }
-        return false;
+        return WHITE;
     }
+
 }
